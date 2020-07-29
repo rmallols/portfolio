ad6ecc5d3fcc0a60b8c785675e8effb126914ae0
Ricardo Mallols <ricardo.mallols@gmail.com>
2020-07-29 16:06:51 +0100

    Simplifying Cypress logic for testing purposes

--- cypress/run-cypress-tests.js
+++
@@ -1,7 +0,0 @@
-const cypress = require('cypress')
-const { start, stop } = require('../scripts/server');
-
-return start().then(async server => {
-    await cypress.run();
-    return stop(server);
-});
